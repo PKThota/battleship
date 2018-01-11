6b9e0027f40cbd0f4197a78c4da151dbe54d9bc5
HarVar-B <harvar009@gmail.com>
2018-01-11 23:23:31 +0530

    modified shipTest.js into mocha framework, using chai.assert.

--- test/ShipTest.js
+++ test/ShipTest.js
@@ -1,31 +1,33 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const Ship = require('../src/Ship.js');
 
-let test = {}
+describe("ship should tell about itself",()=>{
+  beforeEach(()=>{
+    cruiser = new Ship("cruiser",3,[21,22,23]);
+  });
 
-test["isSunk should return true if all the positions of the ship are attacked"] = function(){
-  let cruiser = new Ship("cruiser",3,[21,22,23]);
-  cruiser.damagedAt(21);
-  cruiser.damagedAt(22);
-  cruiser.damagedAt(23);
-  assert.ok(cruiser.isSunk());
-}
+  it("should tell whether it is sink or not",()=>{
+    cruiser.damagedAt(21);
+    cruiser.damagedAt(22);
+    cruiser.damagedAt(23);
+    assert.isOk(cruiser.isSunk);
+  });
 
-test["isSunk should return false if not all the position of the ship are attacked"] = function(){
-  let cruiser = new Ship("cruiser",3,[21,22,23]);
-  cruiser.damagedAt(21);
-  cruiser.damagedAt(22);
-  assert.ok(!cruiser.isSunk());
-}
+  it("should return false for isSunk when it isnot sunk yet.",()=>{
+    cruiser.damagedAt(21);
+    cruiser.damagedAt(22);
+    assert.isNotOk(cruiser.isSunk);
+  });
 
-test["areYouAt should return true if the ship is at that position"] = function(){
-  let BattleShip = new Ship("Battle ship",4,[32,42,52,62]);
-  assert.ok(BattleShip.areYouAt(32))
-}
+  it("should tell whether it is on given position",()=>{
+    assert.isOk(cruiser.areYouAt(22));
+    assert.isNotOk(cruiser.areYouAt(40));
+  });
 
-test["areYouAt should return false if the ship is not at that position"] = function(){
-  let BattleShip = new Ship("Battle ship",4,[32,42,52,62]);
-  assert.ok(!BattleShip.areYouAt(33))
-}
-
-exports.test = test;
+  it("it should store the hit positions",()=>{
+    cruiser.damagedAt(22);
+    cruiser.damagedAt(23);
+    assert.include(cruiser.hitAt,22);
+    assert.include(cruiser.hitAt,23);
+  });
+});
